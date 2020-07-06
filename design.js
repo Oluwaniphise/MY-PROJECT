281b2b172b2317c48f84a8a54f157410a286634a
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-07-06 02:31:44 -0700

    design.js

--- design.js
+++
@@ -1,7 +0,0 @@
-
-   var togglebutton = document.getElementsByClassName("toggle-button")[0];
-   var navLinks = document.getElementsByClassName("nav-links")[0];
-   
-  togglebutton.addEventListener("click", () => {
-    navLinks.classList.toggle("active")
-})
